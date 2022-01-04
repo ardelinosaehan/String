c5f391629f2e354bdec36f28ef63277c922e277b
unknown <saehanardelino@gmail.com>
2022-01-04 20:02:32 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,28 @@
+txt = "Hello World"
+
+# Hitung Jumlah Karakter
+print(len(txt))
+
+# Ambil Karakter Terakhir
+print("txt[11]: ",txt[10])
+
+# Ambil karakter index ke-2 sampai index ke-4 (llo)
+print(txt[2:5])
+
+# Hilangkan spasi pada teks tersebut
+txt = "Hello World"
+
+txt = txt.replace(" ",'')
+print(txt)
+
+# ubah text menjadi huruf besar
+print(txt.upper())
+
+# Ubah text menjadi huruf kecil
+print(txt.lower())
+
+# Ganti karakter H dengan karakter J
+print(txt.replace("H", "J"))
+
+
+
